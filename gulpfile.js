b71b1255458b37234dcfe9a147488cfe3a7ea478
卡晨 <guangbo.hgb@alibaba-inc.com>
2021-09-21 11:33:32 +0800

    feat: add cjs build

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp')
+const babel = require('gulp-babel')
+const del = require('del')
+
+function clean() {
+  return del('./cjs/**')
+}
+
+function buildCJS() {
+  return gulp
+    .src(['es/**/*.{js,jsx}'])
+    .pipe(
+      babel({
+        'plugins': ['@babel/plugin-transform-modules-commonjs'],
+      })
+    )
+    .pipe(gulp.dest('cjs/'))
+}
+
+exports.default = gulp.series(
+  clean,
+  buildCJS,
+)
